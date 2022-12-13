c2dd1f7a5ce1f580a738ee2997b341fd2218c497
Morsul <81906186+Morsul@users.noreply.github.com>
2022-12-13 22:44:37 +0200

    feat: basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  },
+  devServer: {
+    static:{
+      watch: true
+     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+};
