409bf382f506da70e45b73b6c6e8988723eae68f
Svetlana Safonova <safonova.sv.a@gmail.com>
2022-12-25 10:46:15 +0300

    feat: layout cart

--- src/components/element/productList.ts
+++ src/components/element/productList.ts
@@ -3,13 +3,13 @@ import { elementGenerator } from "../controller/taggenerator";
 import { SingleProduct } from "./singleProduct";  
 
 export class ProductList {
-  createProductList(data: Array<IProduct>, classBlock: string):DocumentFragment {
+  createProductList(data: Array<IProduct>, classBlock: string, isShowButton: boolean):DocumentFragment {
     const fragment = new DocumentFragment();
     const mainArticle = elementGenerator.createHTMLElement('article', {className: classBlock});
 
-    data.forEach((item) => {
+    data.forEach((item, index) => {
       const product = new SingleProduct();
-      mainArticle.append(product.createProduct(item));
+      mainArticle.append(product.createProduct(item, index + 1, isShowButton));
     });
 
     fragment.append(mainArticle);
--- src/components/view/cart/cart.ts
+++ src/components/view/cart/cart.ts
@@ -1,14 +1,14 @@
 import { IProduct, View } from "../../../basic";
 import { ProductList } from "../../element/productList";
 import { SummaryCart } from "../../element/summaryCart";
-import './index.css';
+import './index.scss';
 export class Cart implements View {
   draw(data: Array<IProduct>): void {
     const productList = new ProductList();
     const summaryCart = new SummaryCart();
     (<HTMLElement>document.querySelector('.main')).innerHTML = '';
     (<HTMLElement>document.querySelector('.main')).append(
-      productList.createProductList(data, 'cart_list'),
+      productList.createProductList(data, 'cart_list', true),
       summaryCart.createSummeryCart()
     );
   }
