1c8a51ac41dac75de15a8dbccfae27edf7863430
Morsul <81906186+Morsul@users.noreply.github.com>
2022-12-28 21:50:10 +0200

    fix: type.ts fix after merge

--- src/type.ts
+++ src/type.ts
@@ -1,3 +1,46 @@
+export interface View {
+  draw(date: Array<IProduct> | IProduct): void;
+}
+
+export interface Callback<T> {
+  (data: T, options?: IFilter): void;
+}
+export interface IProduct {
+  id: string,
+  title: string,
+  description: string,
+  price: number,
+  discount: number,
+  rating: number,
+  stock: number,
+  brand: string,
+  category: string,
+  thumbnail: string,
+  images: Array<string>
+}
+
+export interface ICatalog {
+  products: Array<IProduct>
+}
+
+export interface ISearchProducts {
+  category?: Array<string>
+}
+
+export interface IFilter {
+  product?: string,
+  category?: string,
+  brand?: string;
+  price?: string,
+  stock?: string
+  sort?: string,
+}
+
+export enum SortType { DESC = 'DESC', ASC = 'ASC'}
+
+export interface ILocalStorageproduct extends Pick<IProduct, "id" | "price" | "discount"> {
+  count: number; 
+}
 
 export interface IPromoCode {
   id: string,
