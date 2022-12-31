4396f36b20549f3a70ed29f01658ea4594eb524d
Morsul <81906186+Morsul@users.noreply.github.com>
2022-12-31 16:24:14 +0200

    feat: pulled changes from development branch

--- src/components/element/productListHeader.ts
+++ src/components/element/productListHeader.ts
@@ -1,53 +1,61 @@
-import { elementGenerator } from "../controller/taggenerator";
-import { FilterControler } from "../controller/filterController";
-import { IFilter } from "../../type";
+import { elementGenerator } from '../controller/taggenerator';
+import { FilterControler } from '../controller/filterController';
+import { IFilter } from '../../type';
 
-export class ProductsHeadline{
+export class ProductsHeadline {
   private _filtredProductCount: HTMLParagraphElement;
   private _search: HTMLInputElement;
   private _filterController: FilterControler;
-  constructor(){
-    this._filtredProductCount = elementGenerator.createParagraph({className: 'shown-count', text: 'Found 0 products'});
-    this._search = elementGenerator.createInput('text', {className: 'search', placeholder: 'Type category or brand name'});
+  constructor() {
+    this._filtredProductCount = elementGenerator.createParagraph({
+      className: 'shown-count',
+      text: 'Found 0 products',
+    });
+    this._search = elementGenerator.createInput('text', {
+      className: 'search',
+      placeholder: 'Type category or brand name',
+    });
     this._filterController = new FilterControler();
   }
 
-  createProdHeader(options?: IFilter): HTMLDivElement{
-    const prodHeaderWrap = elementGenerator.createDiv({className: 'products-headline'});
+  createProdHeader(options?: IFilter): HTMLDivElement {
+    const prodHeaderWrap = elementGenerator.createDiv({ className: 'products-headline' });
 
-    if(options?.allsearch){
+    if (options?.allsearch) {
       this._search.value = options.allsearch;
     }
     prodHeaderWrap.append(this._filtredProductCount, this.searchComponent());
     return prodHeaderWrap;
   }
-  
-  shownCount(number: number): void{
+
+  shownCount(number: number): void {
     this._filtredProductCount.innerText = `Found ${number} products`;
   }
 
-  private overalSearch(): void{
+  private overalSearch(): void {
     const val = this._search.value;
-    if (val.length === 0){
+    if (val.length === 0) {
       this._filterController.removeFilter('allsearch', '*');
     } else {
       this._filterController.addFilter('allsearch', this._search.value, true);
-    }    
+    }
   }
 
-  private searchComponent(): HTMLDivElement{
-    const searchWrap = elementGenerator.createDiv({className: 'search-wrap'});
-    const searchLabel = elementGenerator.createLabel({className: 'search-label'});
+  private searchComponent(): HTMLDivElement {
+    const searchWrap = elementGenerator.createDiv({ className: 'search-wrap' });
+    const searchLabel = elementGenerator.createLabel({ className: 'search-label' });
 
-    searchLabel.addEventListener('click', ()=>{
-      this._search.value='';
+    searchLabel.addEventListener('click', () => {
+      this._search.value = '';
       this.overalSearch();
     });
 
-    this._search.addEventListener('keyup', ()=>{this.overalSearch()});
+    this._search.addEventListener('keyup', () => {
+      this.overalSearch();
+    });
 
     searchWrap.append(this._search, searchLabel);
 
     return searchWrap;
   }
-} 
+}
--- src/components/element/singleProduct.ts
+++ src/components/element/singleProduct.ts
@@ -1,11 +1,11 @@
-import { IProduct } from "../../type";
-import { elementGenerator } from "../controller/taggenerator";
-import Router from "../controller/router";
-import { ILocalStorageproduct, ISingleProductTag} from "../../type";
-import { LocalStorageManager } from "../controller/localStorage";
+import { IProduct } from '../../type';
+import { elementGenerator } from '../controller/taggenerator';
+import Router from '../controller/router';
+import { ILocalStorageproduct, ISingleProductTag } from '../../type';
+import { LocalStorageManager } from '../controller/localStorage';
 
 export class SingleProduct {
-  protected _productAdded: boolean ;
+  protected _productAdded: boolean;
   protected _localStorage;
   protected _isIncrease: boolean;
   private _tagList: ISingleProductTag;
@@ -18,34 +18,43 @@ export class SingleProduct {
     this._item = item;
 
     this._tagList = {
-      image: elementGenerator.createImg(item.thumbnail, {alt: item.title}),
-      title: elementGenerator.createParagraph({text: `${item.title}`, className: 'product-title'}),
-      description:  elementGenerator.createParagraph({text: `${item.description}`, className: 'cart_list-description'}),
-      category: elementGenerator.createParagraph({text: `Category: ${item.category}`}),
-      brand: elementGenerator.createParagraph({text: `Brand: ${item.brand}`}),
-      rating: elementGenerator.createParagraph({text: `Rating: ${item.rating} `}),
-      stock: elementGenerator.createParagraph({text: `Stock: ${item.stock} `}),
-      price: elementGenerator.createParagraph({text: `Price: ${item.price} `, className: 'product-price'}),
-      discount: elementGenerator.createParagraph({text: `Discount: ${item.discount} %`}),
-      addToCart: elementGenerator.createParagraph({className: 'button add-to-cart',text: this._isIncrease ? '+' : 'Add to cart'}),
-      removeFromCart: elementGenerator.createParagraph({className: 'button remove-from-cart', text: this._isIncrease ? '-' : 'Remove from cart'}),
-      goToSingle: elementGenerator.createParagraph({className: 'button button_view', text: 'View Product'}),
-    };    
+      image: elementGenerator.createImg(item.thumbnail, { alt: item.title }),
+      title: elementGenerator.createParagraph({ text: `${item.title}`, className: 'product-title' }),
+      description: elementGenerator.createParagraph({
+        text: `${item.description}`,
+        className: 'cart_list-description',
+      }),
+      category: elementGenerator.createParagraph({ text: `Category: ${item.category}` }),
+      brand: elementGenerator.createParagraph({ text: `Brand: ${item.brand}` }),
+      rating: elementGenerator.createParagraph({ text: `Rating: ${item.rating} ` }),
+      stock: elementGenerator.createParagraph({ text: `Stock: ${item.stock} ` }),
+      price: elementGenerator.createParagraph({ text: `Price: ${item.price} `, className: 'product-price' }),
+      discount: elementGenerator.createParagraph({ text: `Discount: ${item.discount} %` }),
+      addToCart: elementGenerator.createParagraph({
+        className: 'button add-to-cart',
+        text: this._isIncrease ? '+' : 'Add to cart',
+      }),
+      removeFromCart: elementGenerator.createParagraph({
+        className: 'button remove-from-cart',
+        text: this._isIncrease ? '-' : 'Remove from cart',
+      }),
+      goToSingle: elementGenerator.createParagraph({ className: 'button button_view', text: 'View Product' }),
+    };
   }
 
   get tagList() {
     return this._tagList;
   }
 
-  get item () {
+  get item() {
     return this._item;
   }
 
   protected addProduct(arg: IProduct): void {
     const cartLocal: Array<ILocalStorageproduct> = this._localStorage.getLSCart();
     if (this._productAdded) {
-      cartLocal.forEach(e=> {
-        if(e.id === arg.id && e.count < arg.stock) {
+      cartLocal.forEach((e) => {
+        if (e.id === arg.id && e.count < arg.stock) {
           e.count += 1;
         }
       });
@@ -55,39 +64,40 @@ export class SingleProduct {
         id: arg.id,
         count: 1,
         price: arg.price,
-        discount: arg.discount
-      })
+        discount: arg.discount,
+      });
     }
 
-    localStorage.setItem("SACart", JSON.stringify(cartLocal));
-    window.dispatchEvent( new Event('storage') );
+    localStorage.setItem('SACart', JSON.stringify(cartLocal));
+    window.dispatchEvent(new Event('storage'));
   }
-  
-  protected removeProduct(id: string): void { //todo remove return
+
+  protected removeProduct(id: string): void {
+    //todo remove return
     const cartLocal: Array<ILocalStorageproduct> = this._localStorage.getLSCart();
     let t = -1;
 
     if (cartLocal.length > 0) {
       cartLocal.forEach((e, i) => {
-        if(e.id === id) {
+        if (e.id === id) {
           e.count -= 1;
           if (!this._isIncrease) {
             e.count = 0;
           }
         }
-        if(e.count <= 0) {
+        if (e.count <= 0) {
           t = i;
         }
       });
     }
 
-    if (t >= 0){
-      cartLocal.splice(t,1);
+    if (t >= 0) {
+      cartLocal.splice(t, 1);
       this._productAdded = false;
     }
 
-    localStorage.setItem("SACart", JSON.stringify(cartLocal));
-    window.dispatchEvent( new Event('storage') );
+    localStorage.setItem('SACart', JSON.stringify(cartLocal));
+    window.dispatchEvent(new Event('storage'));
     if (!this._productAdded && this._isIncrease) {
       Router.getInstance().routeDefault(`/cart`);
     }
@@ -95,10 +105,17 @@ export class SingleProduct {
 
   public createProduct(): HTMLDivElement {
     const product = elementGenerator.createDiv({});
-    product.append(this.tagList.title, this.tagList.description, this.tagList.category, this.tagList.brand, this.tagList.rating,
-                    this.tagList.stock, this.tagList.price, this.tagList.discount, this.tagList.goToSingle
-                  )
+    product.append(
+      this.tagList.title,
+      this.tagList.description,
+      this.tagList.category,
+      this.tagList.brand,
+      this.tagList.rating,
+      this.tagList.stock,
+      this.tagList.price,
+      this.tagList.discount,
+      this.tagList.goToSingle
+    );
     return product;
-  }  
-
+  }
 }
